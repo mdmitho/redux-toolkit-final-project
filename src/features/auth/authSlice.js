9724ec551c82d4b13aed9569c9125ab11570517a
Awlad Hossen <mdawladhossenmito199922@gmail.com>
2023-01-17 11:56:40 +0600

    Handling user signup (Email Password)

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -10,9 +10,9 @@ const initialState = {
   error:"",
 }
 
-const createUser = createAsyncThunk("auth/createUser",async({email,password})=>{
+export const createUser = createAsyncThunk("auth/createUser",async({email,password})=>{
   const data = await createUserWithEmailAndPassword(auth,email,password)
-  return data;
+  return data.user.email;
 })
 
 
